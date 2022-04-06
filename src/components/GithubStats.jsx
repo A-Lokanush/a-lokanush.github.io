826b0896e602692b47fadb353b634c039946f24e
A-Lokanush <ananthanlokanush@gmail.com>
2022-04-06 17:09:38 +0530

    migrate

--- src/components/GithubStats.jsx
+++ src/components/GithubStats.jsx
@@ -1,51 +1,164 @@
-import React, { useEffect, useState } from 'react';
+import React, { PureComponent, useEffect, useState } from 'react';
 import "./../styles/GithubStats.css";
-import { Treemap, ResponsiveContainer } from 'recharts';
+import { Tooltip, Treemap, ResponsiveContainer } from 'recharts';
 import axios from "axios";
 
-const data = [];
+const data = [
+  {
+    name: 'axis',
+    children: [
+      { name: 'Axes', size: 1302 },
+      { name: 'AxisGridLine', size: 652 },
+      { name: 'AxisLabel', size: 636 },
+      { name: 'CartesianAxes', size: 6703 },
+    ],
+  },
+  {
+    name: 'controls',
+    children: [
+      { name: 'AnchorControl', size: 2138 },
+      { name: 'ClickControl', size: 3824 },
+      { name: 'Control', size: 1353 },
+      { name: 'ControlList', size: 4665 },
+      { name: 'DragControl', size: 2649 },
+      { name: 'IControl', size: 763 },
+      { name: 'PanZoomControl', size: 5222 },
+    ],
+  },
+  {
+    name: 'data',
+    children: [
+      { name: 'EdgeSprite', size: 3301 },
+      { name: 'NodeSprite', size: 19382 },
+      {
+        name: 'render',
+        children: [
+          { name: 'ArrowType', size: 698 },
+          { name: 'EdgeRenderer', size: 5569 },
+          { name: 'IRenderer', size: 353 },
+          { name: 'ShapeRenderer', size: 2247 },
+        ],
+      },
+      { name: 'Tree', size: 7147 },
+      { name: 'TreeBuilder', size: 9930 },
+    ],
+  },
+  {
+    name: 'events',
+    children: [
+      { name: 'SelectionEvent', size: 6880 },
+      { name: 'VisualizationEvent', size: 2117 },
+    ],
+  },
+  {
+    name: 'legend',
+    children: [
+      { name: 'LegendItem', size: 4614 },
+      { name: 'LegendRange', size: 10530 },
+    ],
+  },
+  {
+    name: 'operator',
+    children: [
+      {
+        name: 'distortion',
+        children: [
+          { name: 'BifocalDistortion', size: 4461 },
+          { name: 'FisheyeDistortion', size: 3444 },
+        ],
+      },
+      {
+        name: 'encoder',
+        children: [
+          { name: 'ColorEncoder', size: 3179 },
+          { name: 'ShapeEncoder', size: 1690 },
+          { name: 'SizeEncoder', size: 1830 },
+        ],
+      },
+      {
+        name: 'filter',
+        children: [
+          { name: 'GraphDistanceFilter', size: 3165 },
+          { name: 'VisibilityFilter', size: 3509 },
+        ],
+      },
+      { name: 'IOperator', size: 1286 },
+      {
+        name: 'label',
+        children: [
+          { name: 'RadialLabeler', size: 3899 },
+          { name: 'StackedAreaLabeler', size: 3202 },
+        ],
+      },
+      {
+        name: 'layout',
+        children: [
+          { name: 'AxisLayout', size: 6725 },
+          { name: 'BundledEdgeRouter', size: 3727 },
+          { name: 'RandomLayout', size: 870 },
+          { name: 'StackedAreaLayout', size: 9121 },
+          { name: 'TreeMapLayout', size: 9191 },
+        ],
+      },
+      { name: 'Operator', size: 2490 },
+      { name: 'OperatorList', size: 5248 },
+      { name: 'SortOperator', size: 2023 },
+    ],
+  },
+];
 
 const GithubStats = () => {
-  const [lang, setLang] = useState([]);
-  const [details, setDetails] = useState([]);
+  // const [lang, setLang] = useState([]);
+  // const [details, setDetails] = useState([]);
 
-  const sleep = (delay) => {
-    return new Promise(resolve => {
-      setTimeout(resolve, delay);
-    });
-  }
+  // const sleep = (delay) => {
+  //   return new Promise(resolve => {
+  //     setTimeout(resolve, delay);
+  //   });
+  // }
 
-  useEffect(() => {
-    (async () => {
-      const response = await axios.get(`https://api.github.com/users/A-Lokanush/repos`);
-      const res = response.data;
-      setDetails(res);
-      console.log(details);
-    })();
-  },[]);
+  // useEffect(() => {
+  //   (async () => {
+  //     const response = await axios.get(`https://api.github.com/users/A-Lokanush/repos`);
+  //     const res = response.data;
+  //     setDetails(res);
+  //     console.log(details);
+  //   })();
+  // },[]);
 
-  useEffect(() => {
-    // (async () => {
-      details.map(detail => {
-        (async () => {
-          if(!detail.fork){
-            const response = await axios.get(`https://api.github.com/repos/A-Lokanush/${detail.name}/languages`);
-            let child = [];
-            const res = response.data;
-            Object.entries(res).map((langs) => {
-              child.push({name:langs[0], size:langs[1]})
-            })
-            const obj = {
-              name : detail.name,
-              children : child
-            }
-            data.push(obj);
-            console.log(data);
-          }
-        })();
-      })
-    // })();
-  },[details]);
+  // useEffect(() => {
+  //   (async () => {
+  //     details.map(detail => {
+  //       (async () => {
+  //         if(!detail.fork){
+  //           const response = await axios.get(`https://api.github.com/repos/A-Lokanush/${detail.name}/languages`);
+  //           let child = [];
+  //           const res = response.data;
+  //           Object.entries(res).map((langs) => {
+  //             child.push({name:langs[0], size:langs[1]})
+  //           })
+  //           const obj = {
+  //             name : detail.name,
+  //             children : child
+  //           }
+  //           data.push(obj);
+  //           console.log(data);
+  //         }
+  //       })();
+  //     })
+  //   })();
+  // },[details]);
+
+  //   useEffect(() => {
+  //   const wait = async () => {
+  //     await sleep(5000);
+  //     setLang(data);
+  //     console.log(lang);
+  //   }
+  //   wait();
+  // }, []);
+
+  ////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
   // useEffect(() => {
   //   // (async () => {
@@ -101,19 +214,61 @@ const GithubStats = () => {
   //   console.log(details);
   // },[])
 
-  useEffect(() => {
-    const wait = async () => {
-      await sleep(5000);
-      setLang(data);
-      console.log(lang);
-    }
-    wait();
-  }, []);
+const COLORS = ['#A689E1', '#d896ff', '#be29ec', '#800080', '#660066', '#663399'];
+
+class CustomizedContent extends PureComponent {
+  render() {
+    const { root, depth, x, y, width, height, index, colors, name } = this.props;
+
+    return (
+      <g>
+        <rect
+          x={x}
+          y={y}
+          width={width}
+          height={height}
+          style={{
+            fill: depth < 2 ? colors[Math.floor((index / root?.children?.length) * 6)] : 'none',
+            stroke: '#121212',
+            strokeWidth: 3 / (depth + 1e-10),
+            strokeOpacity: 1 / (depth + 1e-10),
+          }}
+        />
+        {depth === 1 ? (
+          <text x={x + width / 2} y={y + height / 2 + 7} textAnchor="middle" fill="#fff" fontSize={20}>
+            {name}
+          </text>
+        ) : null}
+        {depth === 1 ? (
+          <text x={x + 4} y={y + 18} fill="#800000" fontSize={15} fillOpacity={0.9}>
+            {index + 1}
+          </text>
+        ) : null}
+      </g>
+    );
+  }
+}
+const CustomTooltip = ({ active, payload }) => {
+  
+  if (active && payload && payload.length) {
+    console.log(payload,active)
+    return (
+      <div className="treemap-custom-tooltip">
+        <p>{`${payload[0].payload.root.name}`}</p>
+        <p>{`${payload[0].payload.name} : ${payload[0].value}`}</p>
+      </div>
+    );
+  }
+
+  return null;
+};
 
   return (
     <>
-        <ResponsiveContainer width="50%" height="50%">
-          <Treemap width={400} height={300} data={lang} dataKey="size" ratio={3 / 3} stroke="#fff" fill="#8884d8" />
+        <ResponsiveContainer width="100%" height="100%">
+          <Treemap width={400} height={300} data={data} dataKey="size" ratio={3 / 3} stroke="#fff" fill="#8884d8" content={<CustomizedContent colors={COLORS} />} >
+            <Tooltip content={<CustomTooltip />} />
+          </Treemap>
         </ResponsiveContainer>
     </>    
   )
